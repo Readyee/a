c7054a134c1c313039773d8d8076c9f61ad70eae
Readyee <yesthatsmyemail321@gmail.com>
2024-09-30 17:28:26 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function ok(){
+    var username = prompt("how was is", "say bad or good")
+    if (username == "good"){
+        alert("glad to help :D")
+    }
+    if (username == "Good"){
+        alert("glad to help :D")
+    }
+    if (username == "bad"){
+        alert("im sorry please report it. and thank you")
+    }
+    if (username == "Bad"){
+        alert("im sorry please report it. and thank you")
+    }
+}
